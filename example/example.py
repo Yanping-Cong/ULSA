f2ff685e0f0fce6b3a95524e9c4102858cf4fb8c
Yanping-Cong <ypcong@bao.ac.cn>
2021-04-06 10:00:54 +0800

    new

--- example/example.py
+++ example/example.py
@@ -1,25 +1,16 @@
 from ULSA.sky_map.produce_absorbed_sky_map import absorption_JRZ
-import healpy as hp
 import numpy as np
-import matplotlib.pyplot as plt
-
-def produce_sky_map():
-    sky_map_list = []
-    # initial params setting
-    nside = 2**6
-    dist = 50.
-    for v in range(1,10,0.1):
-        f = absorption_JRZ(v = v, nside = nside, clumping_factor = 1., index_type = 'constant_index_minus_I_E', distance = dist, test = False, emi_form  = 'exp',I_E_form = 'seiffert',R0_R1_equal=True,using_raw_diffuse = False,using_default_params=False,critical_dis = False,output_absorp_free_skymap = False)
-        sky_map_list.append(f.mpi())
-    # we got a list of sky_map with frequency from 1Mhz to 10Mhz with step 0.1Mhz.
-    # then plot the data using mollview
-    return sky_map_list
- 
-def plot():
-    sky_map_list = produce_sky_map()
-    plt.figure(1)
-    for sky_map in sky_map_list
-        hp.mollview(np.log10(sky_map),cmap = plt.cm.jet)
-        plt.show() # or plt.savefig('xxx.eps',format='eps')
-
-plot()
+#input_spectral_index = np.array([2.5])
+import time 
+print ('time_begin',time.ctime())
+#v =1.
+nside = 2**6
+dist = 50.
+if __name__ == '__main__':
+    #cla = absorption_JRZ(v = v, nside = nside,index_type = 'constant_index_minus_I_E', distance = dist,using_raw_diffuse = False,using_default_params=True,input_spectral_index=input_spectral_index)
+    for v in np.arange(0.1,1,0.1):
+    #for v in [1.,3.,10.]:
+        v = round(v,1)
+        cla = absorption_JRZ(v = v, nside = nside,index_type = 'constant_index', distance = dist,using_raw_diffuse = False,using_default_params=True,output_absorp_free_skymap=True,critical_dis = False)
+        cla.mpi()
+        print ('time_end',time.ctime())
